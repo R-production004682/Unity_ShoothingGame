feb121305ac367acd6c643429697669ccb92c5b1
ryousuke <83821881+Ryosuke004682@users.noreply.github.com>
2024-02-21 01:12:30 +0900

    バグ修正

--- CatShoothing/Assets/Reika/Script/ChenseScenes/GR.cs
+++ CatShoothing/Assets/Reika/Script/ChenseScenes/GR.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class scenes : MonoBehaviour
+public class GR : MonoBehaviour
 {
     public void ChengeScene_GR() {
         SceneManager.LoadScene("ResultScene");
