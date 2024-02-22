6fcc1b03cbef10bf6751d12ff0f2a5cf241e8dfc
DESKTOP-JUN795G\reika <reika.reika.9180@gmail.com>
2024-02-23 03:17:50 +0900

    プレイヤーの移動

--- CatShoothing/Assets/Reika/Script/player/PlayerMove.cs
+++ CatShoothing/Assets/Reika/Script/player/PlayerMove.cs
@@ -6,12 +6,15 @@ public class Player : MonoBehaviour
 {
     public float speed = 2.0f;
 
-    private void Update()
+    void Update()
     {
         float x = Input.GetAxisRaw("Horizontal");
-        var position = transform.position;
 
-        position.x = Mathf.Clamp(position.x, -8.0f, 8.0f);
-        transform.position = position;
+        Vector3 currentPosition = transform.position;
+
+        float newXPosition = currentPosition.x + x * Time.deltaTime * speed;
+        newXPosition = Mathf.Clamp(newXPosition, -8.0f, 8.0f);
+
+        transform.position = new Vector2(newXPosition, -4.0f);
     }
 }
