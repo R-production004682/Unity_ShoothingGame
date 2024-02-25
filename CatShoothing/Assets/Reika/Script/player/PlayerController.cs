a21d71d56dab9a2d02960dbb6990540312097c84
ryousuke <83821881+Ryosuke004682@users.noreply.github.com>
2024-02-25 15:32:25 +0900

    射撃システム完成

---
+++ CatShoothing/Assets/Reika/Script/player/PlayerController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float playerMoveSpeed = 2.0f;
+
+    private float interval = .0f;
+    private ObjectPool bulletPool;
+
+    private void Start()
+    {
+        bulletPool = StageContoller.Instance.p_BulletPool;
+    }
+
+
+    void Update()
+    {
+        interval -= Time.deltaTime;
+    }
+
+
+    /// <summary>
+    //  /à⁄ìÆèàóù
+    /// </summary>
+    public void PlayerMovement(float horizontal , Vector3 currentPosition)
+    {
+        currentPosition = this.transform.localPosition;
+        float newXPosition = currentPosition.x + horizontal * Time.deltaTime * playerMoveSpeed;
+        transform.position = new Vector2(newXPosition, -4.0f);
+    }
+
+
+    public void Shot()
+    {
+        if (interval <= .0f)
+        {
+            var bullet = bulletPool.Launch(this.transform.position, 0);
+
+            if (bullet != null)
+                bullet.GetComponent<BulletMove>().speed = 10;
+
+            interval = 1.0f;
+        }
+    }
+}
---
+++ CatShoothing/Assets/Ryosuke/Script/Bullet/ObjectPool/ObjectPool.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+    [SerializeField] PoolContent content = null;
+    [SerializeField] private int maxBullet = 10;
+
+    Queue<PoolContent> bulletQueue;
+
+    private void Start()
+    {
+        bulletQueue = new Queue<PoolContent>(maxBullet);
+
+        for(int i = 0; i < maxBullet; i++)
+        {
+            var templateObj = Instantiate(content);
+            templateObj.transform.parent = transform;
+
+            templateObj.transform.localPosition = new Vector2(30, 30);
+            bulletQueue.Enqueue(templateObj);
+        }
+    }
+
+
+    public PoolContent Launch(Vector3 position , float angle)
+    {
+        if (bulletQueue.Count <= 0) { return null; }
+
+        var temp = bulletQueue.Dequeue();
+        temp.gameObject.SetActive(true);
+        
+        temp.Show(position , angle);
+        return temp;
+    }
+
+
+    public void Collect(PoolContent bullet)
+    {
+        bullet.gameObject.SetActive(false);
+        bulletQueue.Enqueue(bullet);
+    }
+
+
+    public void Reset()
+    {
+        BroadcastMessage($"Hide , {SendMessageOptions.DontRequireReceiver}");
+    }
+}
