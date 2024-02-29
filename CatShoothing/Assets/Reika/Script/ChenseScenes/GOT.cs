23e274aa72c7eb00c5c4cad02563e32a3bd4385d
DESKTOP-JUN795G\reika <reika.reika.9180@gmail.com>
2024-02-29 15:26:59 +0900

    シーン遷移のやつ使わないの消したのとBGMのやつ

---
+++ CatShoothing/Assets/Reika/Script/ChenseScenes/GOT.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GOT : MonoBehaviour
+{
+    private AudioSource se;
+
+    void Start()
+    {
+        se = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    public void OnClick()
+    {
+        se.PlayOneShot(se.clip);
+
+        StartCoroutine(SE_WaitTime());
+    }
+
+    IEnumerator SE_WaitTime()
+    {
+        yield return new WaitForSeconds(2.0f);
+        SceneManager.LoadScene("TitleScene");
+    }
+}
