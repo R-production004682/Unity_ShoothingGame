222946137aac3664063bbb8331139586a72f4fad
ryousuke <83821881+Ryosuke004682@users.noreply.github.com>
2024-02-29 15:31:01 +0900

    最終調整

--- CatShoothing/Assets/Reika/Script/ChenseScenes/GCS.cs
+++ CatShoothing/Assets/Reika/Script/ChenseScenes/GCS.cs
@@ -3,10 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GCS : MonoBehaviour
+public class GCS
 {
     public void ChengeScene_GCS() {
         SceneManager.LoadScene("GameClearScene");
-
     }
 }
--- CatShoothing/Assets/Reika/Script/ChenseScenes/RT.cs
+++ CatShoothing/Assets/Reika/Script/ChenseScenes/RT.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class RT : MonoBehaviour
+public class RT
 {
     public void ChengeScene_RT()
     {
